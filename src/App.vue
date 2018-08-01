07bd1b67b92c83018594eaff84160f1d754d1b31
Zero <339369805@qq.com>
2018-08-01 14:29:36 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <transition :name="transition">
+      <keep-alive>
+        <router-view></router-view>
+      </keep-alive>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+      transition: 'fade'
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
