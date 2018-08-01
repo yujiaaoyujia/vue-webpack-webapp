25597f39c7b7c067fe32ed3eb1b37c989b87921a
Zero <339369805@qq.com>
2018-08-01 14:29:09 +0800

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,51 @@
+import Vue from 'vue'
+import store from './store'
+import router from './router'
+import App from './App'
+
+// 引入全局样式
+import './assets/js/flexible'
+import './assets/css/main.styl'
+import './assets/css/mintUi.styl'
+
+// 引入脚本
+import './assets/js/qingjs'
+import jdy from './assets/js/jdy'
+import yzj from './assets/js/yunzhijia'
+import filters from './assets/js/filters'
+import directive from './assets/js/directive'
+
+window.jdy = jdy
+Vue.prototype.$jdy = jdy
+
+// 关闭生产模式下的提示
+Vue.config.productionTip = false
+// if (!jdy.getParam('debug')) {
+//   console.log = console.error = console.debug = console.warn = console.info = window.alert = jdy.noop
+// }
+
+// 定义 vue 过滤器
+Object.keys(filters).forEach(k => Vue.filter(k, filters[k]))
+// 定义 vue 自定义指令
+Object.keys(directive).forEach(k => Vue.directive(k, directive[k]))
+
+// 全局守卫
+router.beforeEach((to, from, next) => {
+  // 根据路由meta设置title
+  const title = to.meta.title || document.title
+  jdy.setTitle(title)
+  yzj.setTitle(title)
+  next()
+})
+
+// 全局后置钩子
+// router.afterEach((to, from) => {})
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  store,
+  router,
+  template: '<App/>',
+  components: { App }
+})
