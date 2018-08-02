22d39977f2e06f5530cae88712fef086a937cf28
Zero <339369805@qq.com>
2018-08-02 13:59:46 +0800

    Update hack.js

--- src/assets/js/jdy/hack.js
+++ src/assets/js/jdy/hack.js
@@ -1,8 +1,12 @@
 import { isIOS } from './env'
+import yzj from '../yunzhijia'
 
 // 动态设置当前页面的标题
 export default function setTitle(title) {
   document.title = title
+
+  // 动态坑爹的云之家title
+  yzj.setTitle(title)
 
   if (isIOS) {
     const $iframe = $('<iframe src="about:blank" style="display:none;"></iframe>').on('load', () => {
