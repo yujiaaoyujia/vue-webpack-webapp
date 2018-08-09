ad8608312c67fdb4721f1b51951627e80a96b8e5
Zero <339369805@qq.com>
2018-08-09 16:40:12 +0800

    Add files via upload

---
+++ src/views/Page.vue
@@ -0,0 +1,45 @@
+<template>
+  <article class="page">
+    Page
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'Page',
+  data() {
+    return {
+
+    }
+  },
+
+  computed: {
+
+  },
+
+  created() {
+
+  },
+
+  mounted() {
+
+  },
+
+  activated() {
+
+  },
+
+  methods: {
+
+  },
+
+  components: {
+
+  },
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '../assets/css/var.styl'
+
+</style>
