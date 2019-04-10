2ce3f959ee956d9caa1b16d0343bf86a2e40f418
Zero <339369805@qq.com>
2019-04-10 17:07:22 +0800

    Change api in ajax via config
    Add apiPrefix and apiSuffix in config. In order to control the api request address standardly.

--- src/assets/js/jdy/config.js
+++ src/assets/js/jdy/config.js
@@ -1,12 +1,11 @@
 import { getRootPath } from './location'
 
 export default {
-  devStatus: 'release', // 'release',
+  devStatus: 'release', // dev | release,
   devApi: '//' + window.location.host + getRootPath(),
   api: '//' + window.location.host + getRootPath(),
-  apiPrefix: {
-    default: 'api-prefix',
-  },
+  apiPrefix: { default: 'api-prefix' },
+  apiSuffix: { default: '.aspx' },
   version: '1.0.0',
   storageGUID: 'vue-webpack-webapp',
 }
