50a27ed3eabc29b40afceff2963df4330526746c
Zero <339369805@qq.com>
2020-09-18 13:48:01 +0800

    Update directive.js

--- src/assets/js/directive.js
+++ src/assets/js/directive.js
@@ -9,8 +9,9 @@ const longtap = {
 
     const touchStart = (event) => {
       event.stopPropagation()
-      const noPrevent = event.target.className.indexOf('no-prevent')
-      if (noPrevent !== -1) {
+      const eventClass = event.target.className || ''
+      const noPrevent = eventClass.indexOf && eventClass.indexOf('no-prevent')
+      if (noPrevent >= 0) {
         return false
       }
 
@@ -45,8 +46,9 @@ const longtap = {
     }
 
     const touchEnd = (event) => {
-      const noPrevent = event.target.className.indexOf('no-prevent')
-      if (noPrevent !== -1) {
+      const eventClass = event.target.className || ''
+      const noPrevent = eventClass.indexOf && eventClass.indexOf('no-prevent')
+      if (noPrevent >= 0) {
         return false
       }
 
@@ -90,36 +92,42 @@ const menu = {
 }
 
 // css:active扩充, 触发时添加指定类名 v-active
+// 接受修饰符 v-active.bubble 不阻止冒泡
 const active = {
   bind(el, binding) {
     const value = binding.value || 'active'
+    const modifiers = binding.modifiers || {}
+
     const touchStart = (event) => {
-      event.stopPropagation()
+      if (!modifiers.bubble) {
+        event.stopPropagation()
+      }
+
       const eventClass = event.target.className || ''
       const noPrevent = eventClass.indexOf && eventClass.indexOf('no-prevent')
       if (noPrevent >= 0) {
         return false
       }
 
-      if (!el.className) {
-        el.className = value
-      } else if (el.className.indexOf(value) === -1) {
-        el.className = el.className + ' ' + value
+      if (!el.classList.contains(value)) {
+        el.classList.add(value)
       }
       return true
     }
 
     const touchEnd = (event) => {
-      event.stopPropagation()
+      if (!modifiers.bubble) {
+        event.stopPropagation()
+      }
+
       const eventClass = event.target.className || ''
       const noPrevent = eventClass.indexOf && eventClass.indexOf('no-prevent')
       if (noPrevent >= 0) {
         return false
       }
 
-      if (el.className.indexOf(value) >= 0) {
-        const reg = new RegExp('\\s*' + value, 'g')
-        el.className = el.className.replace(reg, '')
+      if (el.classList.contains(value)) {
+        el.classList.remove(value)
       }
       return true
     }
@@ -131,8 +139,72 @@ const active = {
   },
 }
 
+// 单击事件结束后 添加类并在指定时间后移除
+const flash = {
+  bind(el, binding) {
+    let value = binding.value || {}
+    if (typeof value === 'string') {
+      value = { className: value }
+    } else if (typeof value === 'number') {
+      value = { timeOut: value }
+    }
+
+    const className = value.className || 'active'
+    const timeOut = value.timeOut || 200
+
+    const click = () => {
+      if (!el.classList.contains(className)) {
+        el.classList.add(className)
+      }
+      setTimeout(() => {
+        if (el.classList.contains(className)) {
+          el.classList.remove(className)
+        }
+      }, timeOut)
+    }
+
+    el.addEventListener('click', click, { passive: true })
+  },
+}
+
+// 阻止touch事件冒泡引发滚动 v-touch-stop-propagation
+const touchStopPropagation = {
+  bind(el) {
+    let touchY = 0
+    let scrollTop = 0
+    let scrollEnd = 0
+
+    const touchStart = (event) => {
+      const touches = event.targetTouches[0]
+      touchY = touches.clientY
+      scrollTop = el.scrollTop
+      scrollEnd = el.scrollHeight - el.clientHeight
+    }
+
+    const touchMove = (event) => {
+      const touches = event.changedTouches[0]
+      const moveY = touches.clientY - touchY
+
+      // 滚动至顶部时向上滚动
+      if (scrollTop <= 0 && moveY >= 0 && event.cancelable) {
+        event.preventDefault()
+      }
+
+      // 滚动至底部时向下滚动
+      if (scrollTop >= scrollEnd && moveY < 0 && event.cancelable) {
+        event.preventDefault()
+      }
+    }
+
+    el.addEventListener('touchstart', touchStart, { passive: true })
+    el.addEventListener('touchmove', touchMove, { passive: false })
+  },
+}
+
 export default {
   longtap,
   menu,
   active,
+  flash,
+  touchStopPropagation,
 }
